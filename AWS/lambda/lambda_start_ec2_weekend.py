49fdf17cabc409b6dc3a88fb735af1b24ea7533d
Israel Ikier <israel.ikier@assai.com.br>
2023-01-16 15:11:52 -0300

    chore: add new lambda function to start 10% ec2 weekend

--- AWS/lambda/lambda_start_ec2_weekend.py
+++ AWS/lambda/lambda_start_ec2_weekend.py
@@ -1,10 +1,30 @@
 import boto3
+from datetime import datetime
 
 def lambda_handler(event, context):
+    # Connect to EC2
     ec2 = boto3.client('ec2')
-    instance_ids = []
-    # filter instances by tag sleeper=true, start 10% of the instances
-    instances = ec2.describe_instances(Filters=[{'Name': 'tag:sleeper', 'Values': ['true']}])
-    instances = instances['Reservations']
-    instance_ids = [i['Instances'][0]['InstanceId'] for i in instances]
-    ec2.start_instances(InstanceIds=instance_ids[:int(len(instance_ids) * 0.1)])
+    
+    # Get all instances with the tag "sleeper" set to "true"
+    instances = ec2.describe_instances(
+        Filters=[
+            {
+                'Name': 'tag:sleeper',
+                'Values': ['true']
+            }
+        ]
+    )['Reservations']
+    
+    # Get the instance IDs of all instances that are currently stopped
+    stopped_instances = [instance['InstanceId'] for instance in instances if instance['State']['Name'] == 'stopped']
+    
+    # Start 10% of the stopped instances
+    instances_to_start = stopped_instances[:int(len(stopped_instances) * 0.1)]
+    ec2.start_instances(InstanceIds=instances_to_start)
+    
+    # Print the names of the instances that were started
+    started_instances = ec2.describe_instances(InstanceIds=instances_to_start)['Reservations']
+    for instance in started_instances:
+        for tag in instance['Tags']:
+            if tag['Key'] == 'Name':
+                print(tag['Value'])
